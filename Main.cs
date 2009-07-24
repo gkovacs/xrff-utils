8b29b49605ebefd56e9942779d8f3dc759fc49f9
Geza Kovacs <gkovacs@mit.edu>
2009-07-24 01:09:32 -0700

    using file to store tracked features

--- Main.cs
+++ Main.cs
@@ -27,15 +27,14 @@
 using System;
 using System.IO;
 using System.Xml;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace xrffutils
 {
 	class MainClass
 	{
-		public static void mkarff(string xrffFile)
+		public static void mkarff(string xrffFile, string arffFile)
 		{
-			string arffFile =  Regex.Replace(xrffFile, ".xrff$", ".arff");
 			FileStream fso = new FileStream(arffFile, FileMode.Create, FileAccess.Write);
 			StreamWriter ao = new StreamWriter(fso);
 			ao.Write("@relation MindDVDSet");
@@ -136,15 +135,112 @@ namespace xrffutils
 			ao.Close();
 			fso.Close();
 		}
+		
+		public static string[] listfeatures(string featureFile)
+		{
+			FileStream fs = new FileStream(featureFile, FileMode.Open, FileAccess.Read);
+			StreamReader ao = new StreamReader(fs);
+			List<string> features = new List<string>();
+			while (!ao.EndOfStream)
+			{
+				features.Add(ao.ReadLine());
+			}
+			ao.Close();
+			fs.Close();
+			return features.ToArray();
+		}
+		
+		public static void selectfeatures(string featureFile, string inputFile, string outputFile)
+		{
+			selectfeatures(listfeatures(featureFile), inputFile, outputFile);
+		}
+		
+		public static void selectfeatures(string[] features, string inputFile, string outputFile)
+		{
+			// TODO stub
+			foreach (string x in features)
+				Console.WriteLine(x);
+		}
+		
+		public static void addfeatures(string featureFile, string[] allargs)
+		{
+			FileStream fs = new FileStream(featureFile, FileMode.Append, FileAccess.Write);
+			StreamWriter ao = new StreamWriter(fs);
+			for (int i = 2; i < allargs.Length; ++i)
+			{
+				ao.WriteLine(allargs[i]);
+			}
+			ao.Close();
+			fs.Close();
+		}
+		
+		public static void delfeatures(string featureFile, string[] allargs)
+		{
+			string[] allfeatures = listfeatures(featureFile);
+			FileStream fs = new FileStream(featureFile, FileMode.Create, FileAccess.Write);
+			StreamWriter ao = new StreamWriter(fs);
+			foreach (string x in allfeatures)
+			{
+				int i;
+				for (i = 2; i < allargs.Length; ++i)
+				{
+					if (x == allargs[i])
+						break;
+				}
+				if (i == allargs.Length)
+					ao.WriteLine(x);
+			}
+			ao.Close();
+			fs.Close();
+		}
+		
 		public static void Main(string[] args)
 		{
-			if (args.Length < 2)
+			if (args.Length < 1)
 			{
 				Console.WriteLine("not enough arguments");
 				return;
 			}
 			if (args[0] == "mkarff")
-				mkarff(args[1]);
+			{
+				if (args.Length < 3)
+				{
+					Console.WriteLine("not enough arguments for "+args[0]);
+					return;
+				}
+				mkarff(args[1], args[2]);
+			}
+			else if (args[0] == "selectfeatures")
+			{
+				if (args.Length < 4)
+				{
+					Console.WriteLine("not enough arguments for "+args[0]);
+					return;
+				}
+				selectfeatures(args[1], args[2], args[3]);
+			}
+			else if (args[0] == "addfeatures")
+			{
+				if (args.Length < 2)
+				{
+					Console.WriteLine("not enough arguments for "+args[0]);
+					return;
+				}
+				addfeatures(args[1], args);
+			}
+			else if (args[0] == "delfeatures")
+			{
+				if (args.Length < 2)
+				{
+					Console.WriteLine("not enough arguments for "+args[0]);
+					return;
+				}
+				delfeatures(args[1], args);
+			}
+			else
+			{
+				Console.WriteLine("Option "+args[0]+" not recognized");
+			}
 		}
 	}
 }
