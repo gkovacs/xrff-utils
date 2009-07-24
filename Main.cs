1ab36227e40388aa41d1e5e338633e0ed711b6cc
Geza Kovacs <gkovacs@mit.edu>
2009-07-24 15:56:49 -0700

    added attribute selection and validation

--- Main.cs
+++ Main.cs
@@ -27,10 +27,33 @@
 using System;
 using System.IO;
 using System.Xml;
+using System.Xml.Schema;
 using System.Collections.Generic;
 
 namespace xrffutils
 {
+	static class extensions
+	{
+		public static bool Contains<T>(this T[] strl, T item)
+		{
+			foreach (T x in strl)
+			{
+				if (x.Equals(item))
+					return true;
+			}
+			return false;
+		}
+		public static int IndexOf<T>(this T[] strl, T item)
+		{
+			int i;
+			for (i = 0; i < strl.Length; ++i)
+			{
+				if (strl[i].Equals(item))
+					return i;
+			}
+			return -1;
+		}
+	}
 	class MainClass
 	{
 		public static void mkarff(string xrffFile, string arffFile)
@@ -42,90 +65,86 @@ namespace xrffutils
 			ao.WriteLine();
 			FileStream fs = new FileStream(xrffFile, FileMode.Open, FileAccess.Read);
 			XmlTextReader xi = new XmlTextReader(fs);
-			int mode = 0;
 			bool mode1first = true;
 			bool mode2nocomma = true;
 			while (xi.Read())
 			{
-				if (mode == 0)
+				if (xi.Name == "attribute" && xi.IsStartElement())
 				{
-					if (xi.Name == "attribute" && xi.IsStartElement())
+					string attrname = xi.GetAttribute("name");
+					if (attrname != null)
 					{
-						String attrname = xi.GetAttribute("name");
-						if (attrname != null)
+						ao.Write("@attribute "+attrname+" ");
+					}
+					string attrtype = xi.GetAttribute("type");
+					if (attrtype != null)
+					{
+						if (attrtype == "nominal")
 						{
-							ao.Write("@attribute "+attrname+" ");
+							break;
 						}
-						string attrtype = xi.GetAttribute("type");
-						if (attrtype != null)
+						else
 						{
-							if (attrtype == "nominal")
-							{
-								mode = 1;
-							}
-							else
-							{
-								ao.WriteLine(attrtype);
-							}
+							ao.WriteLine(attrtype);
 						}
 					}
 				}
-				else if (mode == 1)
+			}
+			while (xi.Read())
+			{
+				if (xi.Name == "labels")
 				{
-					if (xi.Name == "labels")
+					if (!xi.IsStartElement())
 					{
-						if (!xi.IsStartElement())
-						{
-							mode = 2;
-							ao.Write("}");
-							ao.WriteLine();
-							ao.WriteLine();
-							ao.Write("@data");
-						}
-					}
-					else if (xi.Name == "label" && xi.IsStartElement())
-					{
-						string ltext = xi.ReadElementContentAsString();
-						if (ltext != null)
-						{
-							if (mode1first)
-							{
-								mode1first = false;
-								ao.Write("{");
-							}
-							else
-							{
-								ao.Write(",");
-							}
-							ao.Write(ltext);
-						}
+						ao.Write("}");
+						ao.WriteLine();
+						ao.WriteLine();
+						ao.Write("@data");
+						break;
 					}
 				}
-				else if (mode == 2)
+				else if (xi.Name == "label" && xi.IsStartElement())
 				{
-					if (xi.Name == "instance")
+					string ltext = xi.ReadElementContentAsString();
+					if (ltext != null)
 					{
-						if (xi.IsStartElement())
+						if (mode1first)
 						{
-							mode2nocomma = true;
-						}
-					}
-					else if (xi.Name == "value")
-					{
-						if (mode2nocomma)
-						{
-							ao.WriteLine();
-							mode2nocomma = false;
+							mode1first = false;
+							ao.Write("{");
 						}
 						else
 						{
 							ao.Write(",");
 						}
-						string vtext = xi.ReadElementContentAsString();
-						if (vtext != null)
-						{
-							ao.Write(vtext);
-						}
+						ao.Write(ltext);
+					}
+				}
+			}
+			while (xi.Read())
+			{
+				if (xi.Name == "instance")
+				{
+					if (xi.IsStartElement())
+					{
+						mode2nocomma = true;
+					}
+				}
+				else if (xi.Name == "value")
+				{
+					if (mode2nocomma)
+					{
+						ao.WriteLine();
+						mode2nocomma = false;
+					}
+					else
+					{
+						ao.Write(",");
+					}
+					string vtext = xi.ReadElementContentAsString();
+					if (vtext != null)
+					{
+						ao.Write(vtext);
 					}
 				}
 			}
@@ -143,7 +162,9 @@ namespace xrffutils
 			List<string> features = new List<string>();
 			while (!ao.EndOfStream)
 			{
-				features.Add(ao.ReadLine());
+				string curf = ao.ReadLine().Trim();
+				if (curf != null && curf != string.Empty && !features.Contains(curf))
+					features.Add(curf);
 			}
 			ao.Close();
 			fs.Close();
@@ -158,8 +179,107 @@ namespace xrffutils
 		public static void selectfeatures(string[] features, string inputFile, string outputFile)
 		{
 			// TODO stub
-			foreach (string x in features)
-				Console.WriteLine(x);
+			FileStream fso = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+			StreamWriter tfso = new StreamWriter(fso);
+			FileStream fs = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+			XmlTextReader xi = new XmlTextReader(fs);
+			XmlTextWriter xo = new XmlTextWriter(tfso);
+			xo.Indentation = 1;
+			xo.IndentChar = '\t';
+			xo.Formatting = Formatting.Indented;
+			int[] featureIdx = new int[features.Length];
+			int curidx = 0;
+			while (xi.Read())
+			{
+				if (xi.NodeType == XmlNodeType.Element && xi.IsStartElement())
+				{
+					if (xi.Name == "dataset")
+					{
+						xo.WriteStartElement(xi.Name); // dataset
+						xo.WriteAttributes(xi, true);
+						break;
+					}
+				}
+				else
+				{
+					xo.WriteNode(xi, true);
+				}
+			}
+			while (xi.Read())
+			{
+				if (xi.NodeType == XmlNodeType.Element && xi.IsStartElement())
+				{
+					xo.WriteStartElement(xi.Name);
+					xo.WriteAttributes(xi, true);
+					if (xi.Name == "attributes")
+						break;
+				}
+			}
+			while (xi.Read())
+			{
+				if (xi.IsStartElement())
+				{
+					if (xi.Name == "attribute")
+					{
+						string attrname = xi.GetAttribute("name");
+						if (attrname != null)
+						{
+							if (features.Contains(attrname))
+							{
+								featureIdx[features.IndexOf(attrname)] = curidx;
+								xo.WriteNode(xi, true);
+							}
+							++curidx;
+						}
+					}
+				}
+				else if (xi.NodeType == XmlNodeType.EndElement)
+				{
+					xo.WriteEndElement();
+					if (xi.Name == "header")
+					{
+						break;
+					}
+				}
+			}
+			while (xi.Read())
+			{
+				if (xi.NodeType == XmlNodeType.Element && xi.IsStartElement())
+				{
+					xo.WriteStartElement(xi.Name);
+					xo.WriteAttributes(xi, true);
+					if (xi.Name == "instances")
+						break;
+				}
+			}
+			int vcount = 0;
+			while (xi.Read())
+			{
+				if (xi.Name == "instance")
+				{
+					if (xi.IsStartElement())
+					{
+						vcount = 0;
+						xo.WriteStartElement(xi.Name);
+						xo.WriteAttributes(xi, true);
+					}
+					else if (xi.NodeType == XmlNodeType.EndElement)
+					{
+						xo.WriteEndElement();
+					}
+				}
+				else if (xi.Name == "value" && xi.IsStartElement())
+				{
+					if (featureIdx.Contains(vcount))
+						xo.WriteNode(xi, true);
+					++vcount;
+				}
+			}
+			xi.Close();
+			fs.Close();
+			xo.Close();
+			tfso.Close();
+			fso.Close();
 		}
 		
 		public static void addfeatures(string featureFile, string[] allargs)
@@ -176,7 +296,7 @@ namespace xrffutils
 		
 		public static void delfeatures(string featureFile, string[] allargs)
 		{
-			string[] allfeatures = listfeatures(featureFile);
+			List<string> allfeatures = new List<string>(listfeatures(featureFile));
 			FileStream fs = new FileStream(featureFile, FileMode.Create, FileAccess.Write);
 			StreamWriter ao = new StreamWriter(fs);
 			foreach (string x in allfeatures)
@@ -192,6 +312,55 @@ namespace xrffutils
 			}
 			ao.Close();
 			fs.Close();
+		}
+		
+		public static string[] listfeaturesXrff(string xrffFile)
+		{
+			FileStream fs = new FileStream(xrffFile, FileMode.Open, FileAccess.Read);
+			XmlTextReader xi = new XmlTextReader(fs);
+			List<string> allfeatures = new List<string>();
+			while (xi.Read())
+			{
+				if (xi.IsStartElement())
+				{
+					if (xi.Name == "attribute")
+					{
+						string attrname = xi.GetAttribute("name");
+						if (attrname != null)
+						{
+							allfeatures.Add(attrname);
+						}
+					}
+					else if (xi.Name == "body")
+					{
+						break;
+					}
+				}
+			}
+			xi.Close();
+			fs.Close();
+			return allfeatures.ToArray();
+		}
+		
+		public static void validate(string xrffFile)
+		{
+			FileStream fs = new FileStream(xrffFile, FileMode.Open, FileAccess.Read);
+			XmlReaderSettings s = new XmlReaderSettings();
+			s.ProhibitDtd = false;
+			s.ValidationType = ValidationType.DTD;
+			s.ValidationEventHandler += new ValidationEventHandler(xrffValidationCallback);
+			XmlReader xi = XmlReader.Create(fs, s);
+			while (xi.Read())
+			{
+				
+			}
+			xi.Close();
+			fs.Close();
+		}
+		
+		public static void xrffValidationCallback(object sender, ValidationEventArgs e)
+		{
+			Console.WriteLine("error while validating: "+e.Message);
 		}
 		
 		public static void Main(string[] args)
@@ -210,6 +379,19 @@ namespace xrffutils
 				}
 				mkarff(args[1], args[2]);
 			}
+			else if (args[0] == "listfeatures")
+			{
+				if (args.Length < 2)
+				{
+					Console.WriteLine("not enough arguments for "+args[0]);
+					return;
+				}
+				string[] allfeatures = listfeaturesXrff(args[1]);
+				foreach (string x in allfeatures)
+				{
+					Console.WriteLine(x);
+				}
+			}
 			else if (args[0] == "selectfeatures")
 			{
 				if (args.Length < 4)
@@ -221,7 +403,7 @@ namespace xrffutils
 			}
 			else if (args[0] == "addfeatures")
 			{
-				if (args.Length < 2)
+				if (args.Length < 3)
 				{
 					Console.WriteLine("not enough arguments for "+args[0]);
 					return;
@@ -230,12 +412,21 @@ namespace xrffutils
 			}
 			else if (args[0] == "delfeatures")
 			{
-				if (args.Length < 2)
+				if (args.Length < 3)
 				{
 					Console.WriteLine("not enough arguments for "+args[0]);
 					return;
 				}
 				delfeatures(args[1], args);
+			}
+			else if (args[0] == "validate")
+			{
+				if (args.Length < 2)
+				{
+					Console.WriteLine("not enough arguments for "+args[0]);
+					return;
+				}
+				validate(args[1]);
 			}
 			else
 			{
